ece2e40f5b40a10c9f41a78e8d58b5bd3dc67acb
Diogo <gabriel.san.diogo@gmail.com>
2019-04-14 03:59:54 -0300

    Criação da parte php login no portal do cliente - Não testado

---
+++ portal-cliente/login-form/demo/conection.php
@@ -0,0 +1,5 @@
+<?php 
+
+$strcon=mysqli_connect('db-php.mysql.uhserver.com','alphadir','Alphagsd1316@','db_academia') or die("Error Database");
+
+?>
---
+++ portal-cliente/login-form/demo/verificaLogin.php
@@ -0,0 +1,14 @@
+<?php
+include("conection.php");
+echo "ola teste";
+$login=_POST['txtLogin'];
+$senha=_POST['txtSenha'];
+
+$query=mysqli_query($strcon,"SELECT * FROM usuario_web WHERE usuario='"+ $login + "' AND senha='"+ $senha +"';");
+
+if($query){
+    echo "<script>alert('POST ATUALIZADO!');location.href=\"..\..\..\admin\administrador.html\";</script>";
+}
+
+
+?>
