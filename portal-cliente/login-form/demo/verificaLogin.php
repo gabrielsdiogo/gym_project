a83c46d5b2db42b4b9f65f9addcdccd7dcb9647d
Diogo <gabriel.san.diogo@gmail.com>
2019-04-14 23:03:49 -0300

    Atualização formulario login -portal cliente --Funcionando

--- portal-cliente/login-form/demo/verificaLogin.php
+++ portal-cliente/login-form/demo/verificaLogin.php
@@ -1,13 +1,21 @@
 <?php
 include("conection.php");
 
-$login=_POST['txtLogin'];
-$senha=_POST['txtSenha'];
+$login=$_POST['txtLo'];
+$senha=$_POST['txtSe'];
 
-$query=mysqli_query($strcon,"SELECT * FROM usuario_web WHERE usuario='"+ $login + "' AND senha='"+ $senha +"';");
 
-if($query){
-    echo "<script>alert('POST ATUALIZADO!');location.href=\"..\..\..\admin\administrador.html\";</script>";
+$query=mysqli_query($strcon,"SELECT * FROM usuario_web WHERE usuario='".$login."' AND senha='".$senha."';");
+
+$dados=mysqli_fetch_assoc($query);
+
+
+
+if($dados['usuario']==$login && $dados['senha']==$senha){
+    
+    echo "<script>location.href=\"../../../admin/administrador.html\";</script>";
+}else{
+    echo "nao deu certo";
 }
 
 
