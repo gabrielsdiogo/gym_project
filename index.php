599253378d6eb41d71d0a6c71d94934039ef5ed5
Diogo <gabriel.san.diogo@gmail.com>
2019-05-22 09:03:53 -0300

    updated

--- index.php
+++ index.php
@@ -145,7 +145,7 @@
             <div class="col-lg-4 col-md-6">
               <div class="speaker">
                 <img
-                  src="img/speakers/1.jpg"
+                  src="https://instagram.faep12-1.fna.fbcdn.net/vp/a12340a4fb159d6b4c9c6598cbdbf7f7/5D61FEDE/t51.2885-15/e35/57870513_3128804800466741_4204603153937151521_n.jpg?_nc_ht=instagram.faep12-1.fna.fbcdn.net"
                   alt="Speaker 1"
                   class="img-fluid"
                 />
@@ -164,13 +164,13 @@
             <div class="col-lg-4 col-md-6">
               <div class="speaker">
                 <img
-                  src="img/speakers/2.jpg"
+                  src="https://instagram.faep12-1.fna.fbcdn.net/vp/8dc87badde5b5b77d30c29c8db5b78a6/5D82C57A/t51.2885-15/e35/51593926_380220279472838_5101724665270360003_n.jpg?_nc_ht=instagram.faep12-1.fna.fbcdn.net"
                   alt="Speaker 2"
                   class="img-fluid"
                 />
                 <div class="details">
                   <h3><a href="speaker-details.html">Raony Gois</a></h3>
-                  <p>Um doido qualquer</p>
+                  <p>Arquitetura de banco de dados</p>
                   <div class="social">
                     <a href=""><i class="fa fa-twitter"></i></a>
                     <a href=""><i class="fa fa-facebook"></i></a>
