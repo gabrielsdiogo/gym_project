ffd6821f0fac2b227021187a4069520608605e8d
Diogo <gabriel.san.diogo@gmail.com>
2019-06-07 00:41:22 -0300

    Versao FINAL

--- admin/avisos.php
+++ admin/avisos.php
@@ -1,3 +1,25 @@
+<?php
+      session_start();
+      if(!isset($_SESSION['code'])){
+        session_destroy();
+        echo "<script>location.href=\"../index.php\";</script>";
+        die();
+      }
+    
+      $mysqli = new mysqli('db-academia.mysql.uhserver.com','alphadir','Alphagsd1316@','bd_academia') or die(mysqli_error($mysqli));
+      
+      
+      if(isset($_POST['aviso'])){
+        $aviso=$_POST['aviso'];
+        $data=date("d-m-y");
+        
+        
+        $mysqli->query("INSERT INTO avisos (conteudo,data) VALUES 
+        ('$aviso','$data')") or die($mysqli->error);
+      }
+      
+    
+    ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,35 +37,9 @@
   </head>
 
   <body>
-    <?php
-        session_start();
-        $_SESSION['code']=true;
-        $mysqli = new mysqli('db-academia.mysql.uhserver.com','alphadir','Alphagsd1316@','bd_academia') or die(mysqli_error($mysqli));
-        $result=$mysqli->query("SELECT * FROM alunos") or die($mysqli->error);
-        
-        if(isset($_POST['txtUser'])){
-          $user=$_POST['txtUser'];
-          $pwd=$_POST['txtSenha'];
-          $email=$_POST['txtEmail'];
-          $id=$_POST['id'];
-          $situ=$_POST['radiobt'];
-          
 
-          $mysqli->query("INSERT INTO usuario_web(usuario,senha,email,situacao,idaluno) VALUES 
-          ('$user','$pwd','$email','$situ','$id')") or die($mysqli->error);
-
-          $mysqli->query("UPDATE alunos SET user_web='1' WHERE IDaluno = $id ") or die($mysqli->error);
-          header("refresh: 0");
-        }
-
-        
-        
-    
-        
-    
-    ?>
     <div id="cabecalho" class="cabecalho">
-      <p>Adicionar Usuarios</p>
+      <p>Administrador</p>
       <a id="pushRight" class=""
         ><i class="fa fa-dashboard"></i><span> Menu</span></a
       >
@@ -52,45 +48,24 @@
     <section id="conteudo">
       <div id="conteudo" class="conteudo">
         <div class="header">
-            <h3>Alunos</h3>
+            <h3>avisos</h3>
         </div>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>nome</th>
-              <th>CPF</th>
-              <th colspan="2" class="bot">Incluir usuario</th>
-            </tr>
-          </thead>
-          <?php
-                while ($row=$result->fetch_assoc()): ?>
-            <tr>
-              <td><?php echo $row['nome']?></td>
-              <td><?php echo $row['cpf']; ?></td>
-              <td class="bot">
-                  <?php
-                      
-                      if($row['user_web']==1){
-                        echo "<a style='color:blue; text-decoration:none; cursor:default;'>Adicionado</a>";
-                      }else{
-                        
-                        
-                        echo "<a href='processa.php?id=".$row['IDaluno']."&name=".$row['nome']."' class='btn btn-info'>Adicionar</a>";
-                      }
-                  
-                  
-                  ?>
-              
-                
-              </td>
-            </tr>
-          <?php endwhile ?>
-        </table>
+       <form action="avisos.php" method="post">
+            <input type="text" name="aviso" placeholder="aviso">
+            <input type="submit" value="inserir">
+       </form>
       </div>
+      <div class="tabela-add anime">
+          <div class="header">
+            <h3>Avisos inseridos</h3>
+          </div>
+        
+        
 
       
+      </div>
+      
     </section>
-
     <section class="animate-menu animate-menu-left">
       <ul class="sidebar-menu">
         <li class="sidebar-header">MAIN NAVIGATION</li>
@@ -101,7 +76,7 @@
           </a>
           <ul class="sidebar-submenu">
             <li>
-              <a href="#"
+              <a href="addUsuario.php"
                 ><i class="fa fa-circle-o"></i> Adicionar novo usuario</a
               >
             </li>
@@ -117,89 +92,24 @@
             </li>
           </ul>
         </li>
-        
-          
         <li>
-          <a href="#">
-            <i class="fa fa-share"></i> <span>Sair</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="sidebar-submenu">
-            <li>
-              <a href="logoff.php"><i class="fa fa-circle-o"></i> Sair do Portal</a>
-            </li>
-           
-            <li>
-              <a href="administrador.php"><i class="fa fa-circle-o"></i> Menu Principal</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="../../documentation/index.html"
-            ><i class="fa fa-book"></i> <span>Documentation</span></a
-          >
-        </li>
-
-        <li class="sidebar-header"></li>
-      </ul>
-    </section>
-
-    <section class="animate-menu animate-menu-right">
-      <ul class="sidebar-menu">
-        <li class="sidebar-header">MAIN NAVIGATION</li>
-        <li>
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-            <i class="fa fa-angle-left pull-right"></i>
-          </a>
-          <ul class="sidebar-submenu">
-            <li>
-              <a href="../../index.html"
-                ><i class="fa fa-circle-o"></i> Dashboard v1</a
-              >
-            </li>
-            <li>
-              <a href="../../index2.html"
-                ><i class="fa fa-circle-o"></i> Dashboard v2</a
-              >
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">
+          <a href="avisos.php">
             <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
-            <span class="label label-primary pull-right">4</span>
+            <span>Inserir avisos</span>
+            <!-- <span class="label label-primary pull-right">4</span> -->
           </a>
-          <ul class="sidebar-submenu" style="display: none;">
-            <li>
-              <a href="top-nav.html"
-                ><i class="fa fa-circle-o"></i> Top Navigation</a
-              >
-            </li>
-            <li>
-              <a href="boxed.html"><i class="fa fa-circle-o"></i> Boxed</a>
-            </li>
-            <li>
-              <a href="fixed.html"><i class="fa fa-circle-o"></i> Fixed</a>
-            </li>
-            <li class="">
-              <a href="collapsed-sidebar.html"
-                ><i class="fa fa-circle-o"></i> Collapsed Sidebar</a
-              >
-            </li>
-          </ul>
+       
         </li>
         <li>
           <a href="../widgets.html">
-            <i class="fa fa-th"></i> <span>Widgets</span>
+            <i class="fa fa-th"></i> <span>Imagens</span>
             <small class="label pull-right label-info">new</small>
           </a>
         </li>
         <li>
           <a href="#">
             <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
+            <span>Editar planos</span>
             <i class="fa fa-angle-left pull-right"></i>
           </a>
           <ul class="sidebar-submenu">
@@ -371,67 +281,17 @@
           </ul>
         </li>
         <li>
-          <a href="#">
-            <i class="fa fa-share"></i> <span>Multilevel</span>
-            <i class="fa fa-angle-left pull-right"></i>
+          <a href="logoff.php">
+            <i class="fa fa-share"></i> <span>Sair</span>
           </a>
-          <ul class="sidebar-submenu">
-            <li>
-              <a href="#"><i class="fa fa-circle-o"></i> Level One</a>
-            </li>
-            <li>
-              <a href="#"
-                ><i class="fa fa-circle-o"></i> Level One
-                <i class="fa fa-angle-left pull-right"></i
-              ></a>
-              <ul class="sidebar-submenu">
-                <li>
-                  <a href="#"><i class="fa fa-circle-o"></i> Level Two</a>
-                </li>
-                <li>
-                  <a href="#"
-                    ><i class="fa fa-circle-o"></i> Level Two
-                    <i class="fa fa-angle-left pull-right"></i
-                  ></a>
-                  <ul class="sidebar-submenu">
-                    <li>
-                      <a href="#"><i class="fa fa-circle-o"></i> Level Three</a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fa fa-circle-o"></i> Level Three</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#"><i class="fa fa-circle-o"></i> Level One</a>
-            </li>
-          </ul>
         </li>
         <li>
           <a href="../../documentation/index.html"
             ><i class="fa fa-book"></i> <span>Documentation</span></a
           >
         </li>
-        <li class="sidebar-header">LABELS</li>
 
-        <li>
-          <a href="#"
-            ><i class="fa fa-circle-o text-red"></i> <span>Avisos</span></a
-          >
-        </li>
-        <li>
-          <a href="#"
-            ><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a
-          >
-        </li>
-        <li>
-          <a href="#"
-            ><i class="fa fa-circle-o text-aqua"></i>
-            <span>Information</span></a
-          >
-        </li>
+        <li class="sidebar-header"></li>
       </ul>
     </section>
 
@@ -446,8 +306,6 @@
 
     <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
     <script src="./dist/sidebar-menu.js"></script>
-    <script src="./dist/button-animated.js"></script>
-
     <script>
       $.sidebarMenu($(".sidebar-menu"));
       $.sidebarMenu($(".sidebar-menu-rtl"));
--- index.php
+++ index.php
@@ -83,7 +83,7 @@
         <h1 class="mb-4 pb-0">Projeto<br /><span>Academia </span>Experience</h1>
         <p class="mb-4 pb-0">Estamos te esperando !!</p>
         <a
-          href="https://www.youtube.com/watch?v=QLMHFs9y7Vw"
+          href="https://www.youtube.com/watch?v=svi_zFEFZD4"
           class="venobox play-btn mb-4"
           data-vbtype="video"
           data-autoplay="true"
@@ -437,33 +437,30 @@
                   <h5 class="card-title text-muted text-uppercase text-center">
                     Plano Comum
                   </h5>
-                  <h6 class="card-price text-center">R$150</h6>
+                  <h6 class="card-price text-center">R$79,90</h6>
                   <hr />
                   <ul class="fa-ul">
-                    <li>
-                      <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Regular Seating
-                    </li>
-                    <li>
-                      <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Coffee Break
+                    <li class="text-muted">
+                      <span class="fa-li"><i class="fa fa-times"></i></span
+                      >Acesso ao nosso estacionamento
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Custom Badge
+                      >Aulas de dança
+                    </li>
+                    <li>
+                      <span class="fa-li"><i class="fa fa-check"></i></span
+                      >Spinning
                     </li>
                     <li class="text-muted">
                       <span class="fa-li"><i class="fa fa-times"></i></span
-                      >Community Access
+                      >Acesso ao aplicativo
                     </li>
                     <li class="text-muted">
                       <span class="fa-li"><i class="fa fa-times"></i></span
-                      >Workshop Access
+                      >Acesso ao portal
                     </li>
-                    <li class="text-muted">
-                      <span class="fa-li"><i class="fa fa-times"></i></span
-                      >After Party
-                    </li>
+                    
                   </ul>
                   <hr />
                   <div class="text-center">
@@ -486,33 +483,30 @@
                   <h5 class="card-title text-muted text-uppercase text-center">
                     Plano Premium
                   </h5>
-                  <h6 class="card-price text-center">R$250</h6>
+                  <h6 class="card-price text-center">R$110,50</h6>
                   <hr />
                   <ul class="fa-ul">
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Regular Seating
+                      >Acesso ao nosso estacionamento
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Coffee Break
+                      >Aulas de dança
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Custom Badge
+                      >Spinning
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Community Access
+                      >Acesso ao aplicativo
                     </li>
                     <li class="text-muted">
                       <span class="fa-li"><i class="fa fa-times"></i></span
-                      >Workshop Access
+                      >Acesso ao portal
                     </li>
-                    <li class="text-muted">
-                      <span class="fa-li"><i class="fa fa-times"></i></span
-                      >After Party
-                    </li>
+                    
                   </ul>
                   <hr />
                   <div class="text-center">
@@ -536,32 +530,28 @@
                   <h5 class="card-title text-muted text-uppercase text-center">
                     Plano Pro
                   </h5>
-                  <h6 class="card-price text-center">R$350</h6>
+                  <h6 class="card-price text-center">R$125</h6>
                   <hr />
                   <ul class="fa-ul">
-                    <li>
+                  <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Regular Seating
+                      >Acesso ao nosso estacionamento
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Coffee Break
+                      >Aulas de dança
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Custom Badge
+                      >Spinning
                     </li>
                     <li>
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Community Access
+                      >Acesso ao aplicativo
                     </li>
-                    <li>
+                    <li >
                       <span class="fa-li"><i class="fa fa-check"></i></span
-                      >Workshop Access
-                    </li>
-                    <li>
-                      <span class="fa-li"><i class="fa fa-check"></i></span
-                      >After Party
+                      >Acesso ao portal
                     </li>
                   </ul>
                   <hr />
@@ -571,7 +561,7 @@
                       class="btn"
                       data-toggle="modal"
                       data-target="#buy-ticket-modal"
-                      data-ticket-type="premium-access"
+                      data-ticket-type="Plano PRO"
                     >
                       Compre Agora
                     </button>
@@ -587,7 +577,7 @@
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h4 class="modal-title">Buy Tickets</h4>
+                <h4 class="modal-title">Informe seus dados</h4>
                 <button
                   type="button"
                   class="close"
@@ -604,7 +594,7 @@
                       type="text"
                       class="form-control"
                       name="your-name"
-                      placeholder="Your Name"
+                      placeholder="Seu nome"
                     />
                   </div>
                   <div class="form-group">
@@ -612,7 +602,7 @@
                       type="text"
                       class="form-control"
                       name="your-email"
-                      placeholder="Your Email"
+                      placeholder="Seu Email"
                     />
                   </div>
                   <div class="form-group">
@@ -621,14 +611,14 @@
                       name="ticket-type"
                       class="form-control"
                     >
-                      <option value="">-- Select Your Ticket Type --</option>
-                      <option value="standard-access">Standard Access</option>
-                      <option value="pro-access">Pro Access</option>
-                      <option value="premium-access">Premium Access</option>
+                      <option value="">-- Selecione aqui seu plano --</option>
+                      <option value="standard-access">Plano Comum</option>
+                      <option value="pro-access">Plano Premium</option>
+                      <option value="premium-access">Plano PRO</option>
                     </select>
                   </div>
                   <div class="text-center">
-                    <button type="submit" class="btn">Buy Now</button>
+                    <button type="submit" class="btn">Comprar agora</button>
                   </div>
                 </form>
               </div>
@@ -743,101 +733,12 @@
     Footer
   ============================-->
     <footer id="footer">
-      <div class="footer-top">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-3 col-md-6 footer-info">
-              <img src="img/logo.png" alt="TheEvenet" />
-              <p>
-                In alias aperiam. Placeat tempore facere. Officiis voluptate
-                ipsam vel eveniet est dolor et totam porro. Perspiciatis ad
-                omnis fugit molestiae recusandae possimus. Aut consectetur id
-                quis. In inventore consequatur ad voluptate cupiditate debitis
-                accusamus repellat cumque.
-              </p>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li><i class="fa fa-angle-right"></i> <a href="#">Home</a></li>
-                <li>
-                  <i class="fa fa-angle-right"></i> <a href="#">About us</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i> <a href="#">Services</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="#">Terms of service</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="#">Privacy policy</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li><i class="fa fa-angle-right"></i> <a href="#">Home</a></li>
-                <li>
-                  <i class="fa fa-angle-right"></i> <a href="#">About us</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i> <a href="#">Services</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="#">Terms of service</a>
-                </li>
-                <li>
-                  <i class="fa fa-angle-right"></i>
-                  <a href="#">Privacy policy</a>
-                </li>
-              </ul>
-            </div>
-
-            <div class="col-lg-3 col-md-6 footer-contact">
-              <h4>Contact Us</h4>
-              <p>
-                A108 Adam Street <br />
-                New York, NY 535022<br />
-                United States <br />
-                <strong>Phone:</strong> +1 5589 55488 55<br />
-                <strong>Email:</strong> info@example.com<br />
-              </p>
-
-              <div class="social-links">
-                <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                <a href="#" class="instagram"
-                  ><i class="fa fa-instagram"></i
-                ></a>
-                <a href="#" class="google-plus"
-                  ><i class="fa fa-google-plus"></i
-                ></a>
-                <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      
       <div class="container">
         <div class="copyright">
-          &copy; Copyright <strong>TheEvent</strong>. All Rights Reserved
+          &copy; Copyright <strong>Dealer</strong>. Todos os direitos reservados
         </div>
-        <div class="credits">
-          <!--
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=TheEvent
-        -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
+        
       </div>
     </footer>
     <!-- #footer -->
